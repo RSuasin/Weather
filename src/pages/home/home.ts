ee170d7ee0ccb3897aae6bd62dc04ef528895b2f
rsuasin <roshansuasin@gmail.com>
2017-12-23 17:20:28 -0800

    Changed to dynamic assignment for city and state locations

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
-
+import { Storage } from '@ionic/storage'
 
 
 
@@ -10,9 +10,39 @@ import { WeatherProvider } from '../../providers/weather/weather';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  weather:any;
+  location:{
+    city:string,
+    state:string
+  }
 
-  constructor(public navCtrl: NavController , private weatherProvider:WeatherProvider) {
+  constructor(
+    public navCtrl: NavController , 
+    private weatherProvider: WeatherProvider,
+    private storage:Storage) {
 
   }
 
+  ionViewWillEnter(){
+    this.storage.get('location').then((val) => {
+      if(val != null){
+        this.location = JSON.parse(val);
+      } else{
+
+        this.location = {
+          city: 'Stockton',
+          state: 'CA'
+        }
+      }
+    
+      this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(weather => {
+        this.weather = weather.current_observation;
+      });
+    
+
+    });
+  }
+    
+
+    
 }
